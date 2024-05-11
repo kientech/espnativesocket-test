ec8ebf9e9272b3b24a9e06e198e1b8dea85126d2
Kien Duong Trung <96287101+kientech@users.noreply.github.com>
2024-05-11 08:40:55 +0700

    Update server.js

--- server.js
+++ server.js
@@ -5,6 +5,17 @@ const server = new WebSocket.Server({ port: PORT });
 
 server.on("connection", (socket) => {
   console.log("A new client connected");
+  const sendSensorData = () => {
+    const temperature = Math.floor(Math.random() * (40 - 20 + 1)) + 20; // Giả lập nhiệt độ từ 20°C đến 40°C
+    const humidity = Math.floor(Math.random() * (80 - 50 + 1)) + 50; // Giả lập độ ẩm từ 50% đến 80%
+    const co2 = Math.floor(Math.random() * (800 - 400 + 1)) + 400; // Giả lập CO2 từ 400 ppm đến 800 ppm
+    const sensorData = { temperature, humidity, co2 };
+    ws.send(JSON.stringify(sensorData)); // Chuyển đổi sang chuỗi JSON trước khi gửi
+    console.log(
+      `Temperature: ${temperature} - Humidity: ${humidity} - CO2: ${co2}`
+    );
+  };
+  const sensorDataInterval = setInterval(sendSensorData, 2000);
 
   socket.on("message", (message) => {
     console.log("Received:", message.toString());
